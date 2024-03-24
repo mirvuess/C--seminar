60a47f845883e480afc527fa9ed1b73418e54bf6
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-25 04:05:24 +0500

    add Lesson 3: Task 3

---
+++ Lesson 3/Task 3/Program.cs
@@ -0,0 +1,17 @@
+﻿//homework
+
+//task 1
+
+int number = new Random().Next(1, 999);
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"{number} кратно и 7, и 23.");
+}
+else
+{
+    Console.WriteLine($"{number} не кратно одновременно 7 и 23.");
+}
+
+//task 2
+
