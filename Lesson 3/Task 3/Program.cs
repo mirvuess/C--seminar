002f4dd07bd8ca6259d20371b7e030a44f55060b
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-25 20:40:51 +0500

    add Lesson 3: Task 3 finished homework

--- Lesson 3/Task 3/Program.cs
+++ Lesson 3/Task 3/Program.cs
@@ -15,3 +15,56 @@ else
 
 //task 2
 
+Console.WriteLine("Введите координаты точки (X и Y):");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+
+int quadrant = 0;
+
+if (x > 0 && y > 0)
+    quadrant = 1;
+else if (x < 0 && y > 0)
+    quadrant = 2;
+else if (x < 0 && y < 0)
+    quadrant = 3;
+else if (x > 0 && y < 0)
+    quadrant = 4;
+
+Console.WriteLine($"Точка находится в {quadrant}-й координатной четверти.");
+
+//task 3
+
+Console.WriteLine("Введите целое число из отрезка [10, 99]:");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 10 || num > 99)
+{
+    Console.WriteLine("Число не входит в указанный диапазон.");
+    return;
+}
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+int largestDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+Console.WriteLine($"Наибольшая цифра числа {num} - это {largestDigit}.");
+
+//task 4 
+
+Console.WriteLine("Введите натуральное число:");
+int numBer = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Цифры числа через запятую:");
+
+string numBerString = numBer.ToString();
+
+Console.Write(numBerString[0]);
+
+for (int i = 1; i < numBerString.Length; i++)
+{
+    Console.Write(", " + numBerString[i]);
+}
+
+Console.WriteLine();
+
